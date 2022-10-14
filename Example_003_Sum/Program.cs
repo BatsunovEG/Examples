b4000420bdfcd5a6410d260554055c5f6eb4dbda
Evgeniy <evgeniybatsunov@gmail.com>
2022-10-14 16:11:07 +0300

    ex_003_sum_random

--- Example_003_Sum/Program.cs
+++ Example_003_Sum/Program.cs
@@ -1,4 +1,6 @@
-﻿int numberA = 23;
-int numberB = 47;
+﻿int numberA = new Random().Next(1, 100); // случайное число от 1 до 100
+Console.WriteLine(numberA);
+int numberB = new Random().Next(1, 100);
+Console.WriteLine(numberB);
 int result = numberA + numberB;
 Console.WriteLine(result);
